98a19680a244d463c00bf2c6e64b7a1030aad3a7
naasson-coder <74966852+naasson-coder@users.noreply.github.com>
2021-01-26 22:37:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const MercadoPago = require("mercadopago")
+const app = express()
+
+MercadoPago.configure({
+    sandbox: true,
+    access_token: "TEST-1580772345602321-012700-6291a73b21e75c57ac998c75626a2e15-215173836"
+})
+
+app.get("/", (req, res) => {
+    res.send("Está Funcionando.")
+})
+
+app.get("/pagar", async (req, res) => {
+    var id = "" + Date.now();
+    var emailDoPagador = "naasson_lemos@hotmail.com"
+
+    var dados = {
+        items: [
+            item = {
+                id: id,
+                title: "2x video games",
+                quantity: 1,
+                currency_id: 'BRL',
+                unit_price: parseFloat(150)
+            }
+        ],
+        payer: {
+            email: emailDoPagador
+        },
+        external_reference: id
+    }
+    try{
+        var pagamento = await MercadoPago.preferences.create(dados)
+        console.log(pagamento)
+        //Banco.SalvarPagamento({id: id, pagador: emailDoPagador});
+        return res.redirect(pagamento.body.init_point)
+
+    }catch(err){
+        return res.send(err.messagem)
+    }
+})
+
+app.listen(3000,(req, res) => {
+    console.log("Servidor rodando!")
+})
